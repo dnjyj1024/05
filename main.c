c48674cd8de17c8f4ead91845e78372c36a463a3
dnjyj1024 <dnjyj1024@naver.com>
2022-10-04 16:23:51 +0900

    01

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int num;
+	printf("정수를 입력하세요: ");
+	scanf("%d", &num);
+	if (num>0)
+	printf("양수입니다.");
+	else if (num<0)
+	printf("음수입니다.");
+	else if (num == 0)
+	printf("0 입니다.");
+	
+	return 0;
+	 
+}
